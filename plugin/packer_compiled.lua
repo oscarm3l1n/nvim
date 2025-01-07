556303a7a33be851ff7917ddd934a63d7def4092
Oscar Melin <Oscar.Melin@partner.bmw.de>
2025-01-07 11:54:41 +0100

    change colorscheme

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,10 +89,10 @@ _G.packer_plugins = {
     path = "/Users/omelbmw/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
-  neovim = {
+  ["nightfox.nvim"] = {
     loaded = true,
-    path = "/Users/omelbmw/.local/share/nvim/site/pack/packer/start/neovim",
-    url = "https://github.com/rose-pine/neovim"
+    path = "/Users/omelbmw/.local/share/nvim/site/pack/packer/start/nightfox.nvim",
+    url = "https://github.com/EdenEast/nightfox.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
